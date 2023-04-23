cbd2767b30916ff680def7b5de01971f4e4e3e8f
Ensar Akkuzey <ensarakkuzey@gmail.com>
2023-04-23 23:52:33 +0300

    Add createbook methods.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ function App(){
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
-        console.log("Title: ", title);
+        const updatedBooks = [
+            ...books, 
+            {
+                id: Math.round(Math.random() * 9999),
+                title: title,
+            }
+        ];
+
+        setBooks(updatedBooks);
     };
 
     return (
         <div>
+            {books.length}
             <BookCreate onCreate={createBook} />
         </div>
     );
