cc486cecc68e21c0732e8364f5153f20feacaf6c
Ensar Akkuzey <ensarakkuzey@gmail.com>
2023-04-24 01:05:32 +0300

    Add images for books.

--- src/App.js
+++ src/App.js
@@ -41,6 +41,8 @@ function App(){
 
     return (
         <div className="app">
+            <h1>Reading List</h1>
+            
             <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
             <BookCreate onCreate={createBook} />
         </div>
