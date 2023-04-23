a1403ac4c43b8cfaa4657e5e90d759bc3b5a4fc0
Ensar Akkuzey <ensarakkuzey@gmail.com>
2023-04-24 00:49:05 +0300

    Add edit method and update components.

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,6 +1,24 @@
-function BookEdit(){
+import {useState} from 'react';
+
+function BookEdit({book, onEdit}){
+    const [title, setTitle] = useState(book.title);
+
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onEdit(book.id, title);
+        console.log(title);
+    };
+
     return (
-        <div>BookEdit</div>
+        <form className="book-edit" onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input className="input" value={title} onChange={handleTitleChange}/>
+            <button className="button is-primary">Save</button>
+        </form>
     );
 }
 
