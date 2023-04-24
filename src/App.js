6cf2468ce7e4996ed5a3c8c31af753f423fdea08
Ensar Akkuzey <ensarakkuzey@gmail.com>
2023-04-25 00:17:32 +0300

    Update on JSON-Server data.

--- src/App.js
+++ src/App.js
@@ -26,12 +26,16 @@ function App(){
         setBooks(updatedBooks);
     };
 
-    const editBookById = (id, title) => {
+    const editBookById = async (id, title) => {
+        const response = await axios.put(`http://localhost:3999/books/${id}`, {
+            title: title,
+        });
+
         const updatedBooks = books.map((book) => {
             if(book.id === id){
                 return {
                     ...book,
-                    title: title,
+                    ...response.data
                 };
             }
             return book;
