aa4a986054cd496db33e75b46e532a4e77b99ae1
Ensar Akkuzey <ensarakkuzey@gmail.com>
2023-04-29 02:27:32 +0300

    Create context fil.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,19 @@
+import { useContext } from 'react';
+import BooksContext from '../context/book';
 import BookShow from './BookShow';
 
 function BookList({books, onDelete, onEdit}){
+    const value = useContext(BooksContext);
+
     const renderedBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit}/>
     });
 
     return (
-        <div className="book-list">{renderedBooks}</div>
+        <div className="book-list">
+            {value}
+            {renderedBooks}
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import BooksContext from "./context/book";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+    <BooksContext.Provider value={5}>
+        <App />
+    </BooksContext.Provider>
+    );
